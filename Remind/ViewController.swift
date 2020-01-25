b73a7a9ffbb5c35672db642de2020cd6cd3e482c
Daniel Kim <kdanielive@gmail.com>
2020-01-26 01:52:28 +0900

    committing the whole project

---
+++ Remind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Remind
+//
+//  Created by Daniel Kim on 1/26/20.
+//  Copyright © 2020 Daniel Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
