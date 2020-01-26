3907d7daa1bae85f99d26e2f108c10435f107a90
Daniel Kim <kdanielive@gmail.com>
2020-01-26 16:13:36 +0900

    Set the data

---
+++ Remind/Swift/Globals.swift
@@ -0,0 +1,12 @@
+//
+//  Globals.swift
+//  Remind
+//
+//  Created by Daniel Kim on 1/26/20.
+//  Copyright © 2020 Daniel Kim. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+var currentPerson: String?
