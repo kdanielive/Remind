5650939b88c3b84551f8d1f5b29f450a7ffc15ae
Daniel Kim <kdanielive@gmail.com>
2020-01-26 16:11:07 +0900

    Working on the detail page

--- Remind/AddingView/SecondAddingViewController.swift
+++ Remind/AddingView/SecondAddingViewController.swift
@@ -9,14 +9,32 @@
 import UIKit
 
 class SecondAddingViewController: UIViewController {
+    
+    var annualButtonState = true
 
+    @IBOutlet var annualButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        annualButton.backgroundColor = UIColor.black
+        annualButton.layer.borderColor = UIColor.black.cgColor
+        annualButton.layer.borderWidth = 1
+        annualButton.layer.roundCorners(radius: 10)
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func annualEventButtonTouched(_ sender: UIButton) {
+        if(annualButtonState) {
+            annualButton.backgroundColor = UIColor.clear
+            annualButtonState = false
+        } else {
+            annualButton.backgroundColor = UIColor.black
+            annualButtonState = true
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
