ec3ee962528b7245d683bf7ab67ca30a08b6fbee
Daniel Kim <kdanielive@gmail.com>
2020-02-03 00:16:15 +0900

    as;lkfdj

--- Remind/RelationView/RelationTableViewController.swift
+++ Remind/RelationView/RelationTableViewController.swift
@@ -49,15 +49,15 @@ class RelationTableViewController: UITableViewController {
         }
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "relationcell", for: indexPath) as! RelationTableViewCell
 
         // Configure the cell...
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
